50b886ab45482f11c84259c11912a8be4709a608
jhackett1 <joshua.hackett93@gmail.com>
2018-03-31 19:32:54 +0100

    Function to build api url, config variable for api key

--- server.rb
+++ server.rb
@@ -1,5 +1,11 @@
 require 'sinatra'
 
+# function to create api request URL
+def endpointBuilder(location, keywords)
+  return "http://api.eventful.com/rest/events/search?app_key=" + ENV['APP_KEY'] + "&location=" + location + ",UK&keywords=" + keywords
+end
+
+
 # Home route - uses index.erb view
 get('/') do
   erb :index
@@ -9,6 +15,7 @@ end
 post('/events') do
   puts params[:location]
   puts params[:keyword]
+  puts endpointBuilder(params[:location], params[:keyword], "")
   erb :results
 end
 
