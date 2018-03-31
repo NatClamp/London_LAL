200cc227174dc8b912e38048ea9f0dc3743b2299
jhackett1 <joshua.hackett93@gmail.com>
2018-03-31 18:40:12 +0100

    Created signup/results routes and header/footer partials

--- server.rb
+++ server.rb
@@ -1,17 +1,20 @@
 require 'sinatra'
 
+# Home route - uses index.erb view
 get('/') do
   erb :index
 end
 
+# Results route
 post('/events') do
   puts params[:location]
   puts params[:keyword]
+  erb :results
 end
 
+# Email signup route
 post('/signup') do
   puts params[:name]
   puts params[:email]
-
-  "All OK"
+  erb :thankyou
 end
