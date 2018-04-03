981f71aa00776ff3fb94503e4ca8a79eb5c22f50
Natalie Clamp <nat.clamp@gmail.com>
2018-04-03 22:44:48 +0100

    making progress on trying the api

--- server.rb
+++ server.rb
@@ -1,4 +1,7 @@
 require 'sinatra'
+require 'net/http'
+require 'open-uri'
+require 'nokogiri'
 
 # function to create api request URL
 def endpointBuilder(location, keywords)
@@ -16,6 +19,11 @@ post('/events') do
   puts params[:location]
   puts params[:keyword]
   puts endpointBuilder(params[:location], params[:keyword])
+  # uri = URI.parse(URI.encode(endpointBuilder(params[:location], params[:keyword])))
+  # api_response = Net::HTTP.get(uri)
+  # doc = Nokogiri::XML(open(api_response)) do |config|
+  #   config.options = Nokogiri::XML::ParseOptions::STRICT | Nokogiri::XML::ParseOptions::NOBLANKS
+  # end
   erb :results
 end
 
